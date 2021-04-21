9fcdf36c58801aae53531612d811ed055c609137
Mnabawy <m_ninja61@yahoo.com>
2021-04-21 12:38:09 +0200

    create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -23,7 +24,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -34,7 +35,11 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->save();
     }
 
     /**
